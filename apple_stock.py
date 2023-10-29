1a5b365171789db23a6d2bdef3fd1f34b5425b08
ranil-rai <ranil.rai72@spsmail.cuny.edu>
2023-10-29 19:51:36 -0400

    added script

--- apple_stock.py
+++ apple_stock.py
@@ -1,2 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+def fetch_apple_stock():
+    URL = "https://finance.yahoo.com/quote/AAPL/history?p=AAPL"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    page = requests.get(URL, headers=headers)
+    
+    soup = BeautifulSoup(page.content, 'html.parser')
+    
+    rows = soup.find_all('tr', class_='BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)')
+    
+    print("Date, Close Price")
+    for row in rows:
+        details = row.find_all('td')
+        if len(details) >= 5:  # Check if the row contains enough data
+            date = details[0].get_text(strip=True)
+            close_price = details[4].get_text(strip=True)
+            print(f"{date}, {close_price}")
+
 if __name__ == "__main__":
-    pass
+    fetch_apple_stock()
--- football_stats.py
+++ football_stats.py
@@ -1,2 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+def fetch_football_stats():
+    URL = "https://www.cbssports.com/nfl/stats/player/passing/nfl/regular/qualifiers/"
+    page = requests.get(URL)
+    
+    soup = BeautifulSoup(page.content, 'html.parser')
+    
+    players = soup.find_all('tr', class_='TableBase-bodyTr')
+    
+    print("Player, Team, TDs")
+    for player in players[:20]:
+        details = player.find_all('td')
+        name = details[0].get_text(strip=True)
+        team = details[1].get_text(strip=True)
+        tds = details[6].get_text(strip=True)
+        
+        print(f"{name}, {team}, {tds}")
+
 if __name__ == "__main__":
-    pass
+    fetch_football_stats()
